ca45b61f06678705ce6a1ed739c1abc55113ff25
Cong-Hoang <18hcong@gmail.com>
2025-02-25 01:00:00 +0700

    vid 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -3,11 +3,23 @@ const TodoNew = (props) => {
 
   const {addNewTodo} = props;
 
-  addNewTodo("Công Hoàng");
+  // addNewTodo("Công Hoàng");
+  const handleClick = () => {
+    alert("click me");
+  }; 
+  const handleChange = (e) => {
+    console.log('>>check points', e.target.value);
+  }
   return (
     <div className="todo-new">
-				<input type="text" placeholder="Add new todo list" />
-				<button>Add</button>
+				<input 
+        type="text" 
+        placeholder="Add new todo list" 
+        onChange={(event) => {handleChange(event.target.value)}}
+        />
+				<button 
+        onClick={handleClick}
+        >Add</button>
 			</div>
   );
 }
