42cb00158e576afc327374529ef0d69b46b57836
Cong-Hoang <18hcong@gmail.com>
2025-02-24 21:12:21 +0700

    vid 34

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
+import reactLogo from "./assets/react.svg";
 import "./components/todo/todo.css";
 import TodoData from "./components/todo/TodoData";
 import TodoNew from "./components/todo/TodoNew";
-import reactLogo from "./assets/react.svg";
 
 const App = () => {
+	const conghoang = "Công Hoàng Fan MU";
+	const age = 24;
+	const data = {
+		address: "Ha Noi",
+		phone: "0123456789",
+		country: "Viet Nam",
+	}
 	return (
 		<div className="todo-container">
 			<div className="todo-title">Todo List</div>
 			<TodoNew />
-			<TodoData />
+			<TodoData 
+			name={conghoang} 
+			age={age} 
+			data={data} 
+			/>
 			<div className="'todo-image">
-				<img src={reactLogo}className="logo" alt="React Logo" />
+				<img src={reactLogo} className="logo" alt="React Logo" />
 			</div>
 		</div>
 	);
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,6 +1,12 @@
-const TodoData = () => {
+const TodoData = (props) => {
+
+  //props là một obejct chứa các thuộc tính truyền vào từ component cha.
+
+   const {name, age, data} = props;
+  console.log(">>check props", props);
   return(
     <div className="todo-data">
+      <div>My name is: {name} </div>
 				<div>Leaning React</div>
 				<div>Watching Youtube</div>
 			</div>
